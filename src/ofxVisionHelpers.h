82e4229c637b78a3282ffeadbd6f4dbbdce3f469
Roy Macdonald <macdonald.roy@gmail.com>
2024-04-12 19:19:01 -0400

    cleanup

--- src/ofxVisionHelpers.h
+++ src/ofxVisionHelpers.h
@@ -22,30 +22,3 @@ public:
     
     
 };
-
-class ofxVisionAngleColors {
-public:
-    float red;
-    float blue;
-    float green;
-    
-    static float convert(float value, float minValue, float maxValue) {
-        
-        maxValue = maxValue * 0.8;
-        minValue = minValue + (maxValue * 0.2);
-        
-        if(ofIsFloatEqual(0.0f, maxValue - minValue)){ return 0; } // protect from zero division
-        
-        return ofMap(value, minValue, maxValue, 0, 1, true);
-    }
-    
-    
-    void init(float roll, float pitch, float yaw) {
-        auto pi = glm::pi<float>();
-        auto halfPi = pi *0.5f;
-        red = convert(roll, - pi, pi);
-        blue = convert( pitch, -halfPi,  halfPi);
-        green = convert( yaw, -halfPi, halfPi);
-    }
-};
-
